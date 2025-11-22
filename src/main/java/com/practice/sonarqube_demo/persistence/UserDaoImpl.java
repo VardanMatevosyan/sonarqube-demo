da51473191e801f3b32169c2e8077466f6499096
VardanMatevosyan <vardanmt@gmail.com>
2025-11-22 16:28:50 +0400

    fixing vulnerable code - SQL Injection

--- src/main/java/com/practice/sonarqube_demo/persistence/UserDaoImpl.java
+++ src/main/java/com/practice/sonarqube_demo/persistence/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.practice.sonarqube_demo.persistence;
 
+import com.practice.sonarqube_demo.exception.DaoException;
 import com.practice.sonarqube_demo.model.entities.User;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Optional;
 
 @Repository
@@ -21,18 +19,20 @@ public class UserDaoImpl implements UserDao {
     final DataSource dataSource;
 
     public Optional<User> getUserById(Integer id) {
+
+        String query = "SELECT id, username, email FROM users WHERE id = ?";
         try (
                 Connection connection = dataSource.getConnection();
-                Statement statement = connection.createStatement()) {
-            String query = "SELECT id, username, email FROM users WHERE id = " + id;
-            var rs = statement.executeQuery(query);
+                PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, id);
+            var rs = ps.executeQuery();
             if (rs.next()) {
                 User user = buildUserEntity(rs);
                 return Optional.of(user);
             }
             return Optional.empty();
         } catch (SQLException e) {
-            throw new RuntimeException("SQL error", e);
+            throw new DaoException("SQL error", e);
         }
     }
 
